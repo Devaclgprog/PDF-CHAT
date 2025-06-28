259dfc0f0c6e1d0019108a38345a26bb519c8111
Deva Dharshini <devadharshinid.cloud.mfedu@gmail.com>
2025-06-28 13:37:17 +0530

    Update pdfchat.py

--- pdfchat.py
+++ pdfchat.py
@@ -29,7 +29,7 @@ safety_settings = [
 ]
 
 model = genai.GenerativeModel(
-    model_name="gemini-1.5-pro-latest",
+    model_name="gemini-2.0-flash",
     generation_config=generation_config,
     safety_settings=safety_settings,
 )
